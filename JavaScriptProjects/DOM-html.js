17adbc4c54caf2ec3adb51f9336577e2f2c57769
olsikule <olsi.kule@gmail.com>
2022-12-06 11:08:36 -0500

    DOM classes and intro to event listeners

--- JavaScriptProjects/DOM-html.js
+++ JavaScriptProjects/DOM-html.js
@@ -1,18 +1,32 @@
-const p = document.querySelector("p");
-console.log(p);
-const div = document.querySelectorAll("div")
-const grogu = document.querySelector(".group")
-console.log(grogu)
-console.log(div);
+// const p = document.querySelector("p");
+// console.log(p);
+// const div = document.querySelectorAll("div")
+// const grogu = document.querySelector(".group")
+// console.log(grogu)
+// console.log(div);
 
-// getters vs setters 
-// when using console.dir to select an element on the page this is a called a getter e.g.
-const heading = document.querySelector('.title');
-console.dir(heading.textContent);
-heading.textContent = "This is the chosen div"
-console.log(heading.textContent);
+// // getters vs setters 
+// // when using console.dir to select an element on the page this is a called a getter e.g.
+// const heading = document.querySelector('.title');
+// console.dir(heading.textContent);
+// heading.textContent = "This is the chosen div"
+// console.log(heading.textContent);
 
 // textContent vs innerText
 // textContent shows everything ie. all selected including elements and styles 
 // innerText only shows readable text and is aware of styles and script (maybe inner stands for "what's inside")
+
+
+//classes
+const pic = document.querySelector(".cool")
+console.log(pic.classList);
+pic.classList.replace("nice", "awesome")
+
+function toggleRound(){
+    pic.classList.toggle("round");
+}
+ 
+pic.addEventListener("mouseover",  toggleRound); 
+pic.addEventListener("mouseout",  toggleRound); 
+
 
