2f27e9b1af20f9cbf2d69eb69dcaa0644598251c
olsikule <olsi.kule@gmail.com>
2022-12-07 10:31:24 -0500

    HTML strings and XSS

--- JavaScriptProjects/strings.js
+++ JavaScriptProjects/strings.js
@@ -1,4 +1,9 @@
 console.log("this is another test")
 const item = document.querySelector(".item2");
 
+item.innerHTML = `
+<div>
+    <h1> Hey how are you? </h1>
+</div>
+`
 console.log(item.innerHTML)
