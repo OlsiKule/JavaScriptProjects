80fdc1bd9de5240ba54cee13ba4c781730e3cf84
olsikule <olsi.kule@gmail.com>
2022-12-12 10:30:50 -0500

    woring on classes

--- JavaScriptProjects/DOM-html.js
+++ JavaScriptProjects/DOM-html.js
@@ -1,64 +1,64 @@
-// put the script tag right before the closing body tag </body>
-// also could use fxn init and addEventListener with DOMContentLoaded, this so that we can have script on top of HTML b/f the body tag
+// // put the script tag right before the closing body tag </body>
+// // also could use fxn init and addEventListener with DOMContentLoaded, this so that we can have script on top of HTML b/f the body tag
 
-// q...Selector and selectorAll takes one argument 
-const p = document.querySelector("p");
-console.log(p);
+// // q...Selector and selectorAll takes one argument 
+// const p = document.querySelector("p");
+// console.log(p);
 
-// it selects everything with div in a node list 
-// node list looks like an array but it is NOT an array b/c it doesn't have all the methods like map, filter, reduce build into it
-// can use parent child selector eg. ".item2 .title" where title is the child elmnt
-const divs = document.querySelectorAll("div")
+// // it selects everything with div in a node list 
+// // node list looks like an array but it is NOT an array b/c it doesn't have all the methods like map, filter, reduce build into it
+// // can use parent child selector eg. ".item2 .title" where title is the child elmnt
+// const divs = document.querySelectorAll("div")
 
-// if I want to use a child element in a div then we go .item2 to target the parent 
-// this way it will limit the scope of the search
-const grogu = document.querySelector(".item2")
-const image = grogu.querySelector("img")
-console.log(image)
-
-
-// getters vs setters 
+// // if I want to use a child element in a div then we go .item2 to target the parent 
+// // this way it will limit the scope of the search
+// const grogu = document.querySelector(".item2")
+// const image = grogu.querySelector("img")
+// console.log(image)
 
 
-// when using console.dir to select an element on the page this is a called a getter e.g.
-// console.dir shows the object properties, lot of legacy things
-// some useful ones are parent.element 
-const heading = document.querySelector('.title');
-// this is a getter 
-console.log(heading.textContent);
-// this is a setter ie. when updating text content  property on that element 
-heading.textContent = "This is the chosen div"
-console.log(heading.textContent);
+// // getters vs setters 
 
 
-// textContent vs innerText
-// textContent shows everything ie. all selected including elements and styles 
-// innerText only shows readable text and is aware of styles and script (maybe inner stands for "what's inside")
-console.log(heading.textContent);
-console.log(heading.innerText);
+// // when using console.dir to select an element on the page this is a called a getter e.g.
+// // console.dir shows the object properties, lot of legacy things
+// // some useful ones are parent.element 
+// const heading = document.querySelector('.title');
+// // this is a getter 
+// console.log(heading.textContent);
+// // this is a setter ie. when updating text content  property on that element 
+// heading.textContent = "This is the chosen div"
+// // console.log(heading.textContent);
 
-console.log(heading.innerHTML)
-console.log(heading.outerHTML)
 
-const pizzaList = document.querySelector(".pizza")
-console.log(pizzaList.textContent)
+// // textContent vs innerText
+// // textContent shows everything ie. all selected including elements and styles 
+// // innerText only shows readable text and is aware of styles and script (maybe inner stands for "what's inside")
+// console.log(heading.textContent);
+// console.log(heading.innerText);
 
-// this approach reloads the entire page slowing the app with heavy text
-// pizzaList.textContent = `${pizzaList.textContent} 🍕`
-// this is another method with two argumnt
-pizzaList.insertAdjacentText("afterbegin", "🍕")
+// console.log(heading.innerHTML)
+// console.log(heading.outerHTML)
 
-// //classes
-// const pic = document.querySelector(".cool")
-// console.log(pic.classList);
-// pic.classList.replace("nice", "awesome")
+// const pizzaList = document.querySelector(".pizza")
+// console.log(pizzaList.textContent)
 
-// function toggleRound(){
-//     pic.classList.toggle("round");
-// }
+// // this approach reloads the entire page slowing the app with heavy text
+// // pizzaList.textContent = `${pizzaList.textContent} 🍕`
+// // this is another method with two argumnt
+// pizzaList.insertAdjacentText("afterbegin", "🍕")
+
+//classes 
+const pic = document.querySelector(".cool")
+console.log(pic.classList);
+pic.classList.replace("nice", "awesome")
+
+function toggleRound(){
+    pic.classList.toggle("round");
+}
  
 pic.addEventListener("mouseover",  toggleRound); 
-// pic.addEventListener("mouseout",  toggleRound); 
+pic.addEventListener("mouseout",  toggleRound); 
 
 
 
