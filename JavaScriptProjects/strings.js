871351e1f58cdf00e217f09e80a6adb3ac53fc18
olsikule <olsi.kule@gmail.com>
2022-12-14 08:38:01 -0500

    working on adding HTML

--- JavaScriptProjects/strings.js
+++ JavaScriptProjects/strings.js
@@ -1,9 +1,18 @@
-console.log("this is another test")
-const item = document.querySelector(".item2");
+const item = document.querySelector(".item1");
 
-item.innerHTML = `
-<div>
-    <h1> Hey how are you? </h1>
+// create const then instert them with the help of the backticks into  
+const width = 150;
+const src = `https://picsum.photos/${width}`;
+const description = "cute pup"
+// can dump inner html into the browser directly this way, with backticks we can have mulitple lines unlike // which doesn't allow that
+const myHTML = `
+<div class="wrapper">
+    <h2> Hey ${description}? </h2>
+    <img src="${src}" alt="${description}" />
 </div>
 `
+// the above const is a string see console.log below
+console.log(typeof myHTML)
+ 
+item.innerHTML = myHTML 
 console.log(item.innerHTML)
