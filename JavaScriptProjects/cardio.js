b9cbec2186379b0f84dd034848d19457da0a0b24
olsikule <olsi.kule@gmail.com>
2022-12-14 12:43:51 -0500

    cardio exercises

--- JavaScriptProjects/cardio.js
+++ JavaScriptProjects/cardio.js
@@ -1,30 +1,45 @@
-const string = `
-<div class="wrapper">
-<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis minus tenetur eum ut eaque expedita dolor officia consequuntur error, quod qui rem itaque explicabo pariatur recusandae iusto quos nihil labore.</p>
-<p>Lorem ipsum dolor sit amet.</p>
-</div>
-`
+console.log("this is a cardio test")
+// Make a div
 
-// const fragment = document.createRange().createContextualFragment(string);
-string.innerHTML = string;
+// add a class of wrapper to it
 
-const addClass = document.querySelector(".wrapper p")
+// put it into the body
 
-console.log(addClass)
+// make an unordered list
 
+// add three list items with the words "one, two, three" in them
+// put that list into the above wrapper
 
-const playerCard = document.querySelector(".string")
-function generatePlayerCard(name, age, height) {
-    const heightInIches = height / 2.54
-    playerCard.innerHTML = `
-<div class="playerCard">
-    <h2>${name} - ${age} </h2>
-    <p>They are ${heightInIches } and ${age} years old. In dog years this person would be ${age * 7}. That would be a tall dog!</p>
-</div>
-    `
+// create an image
 
-}
-generatePlayerCard("dogo", 5, 139)
-// console.log(generatePlayerCard())
+// set the source to an image
+// set the width to 250
+// add a class of cute
+// add an alt of Cute Puppy
+// Append that image to the wrapper
 
-  
+// with HTML string, make a div, with two paragraphs inside of it
+// put this div before the unordered list from above
+
+// add a class to the second paragraph called warning
+// remove the first paragraph
+
+// create a function called generatePlayerCard that takes in three arguments: name, age, and height
+
+// have that function return html that looks like this:
+// <div class="playerCard">
+//   <h2>NAME — AGE</h2>
+//   <p>They are HEIGHT and AGE years old. In Dog years this person would be AGEINDOGYEARS. That would be a tall dog!</p>
+// </div>
+
+// make a new div with a class of cards
+
+// make 4 player cards using generatePlayerCard
+
+// append those cards to the div
+// put the div into the DOM just before the wrapper element
+// Bonus, put a delete Button on each card so when you click it, the whole card is removed
+
+// select all the buttons!
+// make out delete function
+// loop over them and attach a listener
