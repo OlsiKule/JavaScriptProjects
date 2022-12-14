0696603a7322ad382a0ed2d4b9c0833b8a30149d
olsikule <olsi.kule@gmail.com>
2022-12-14 09:01:38 -0500

    HTML from string and XSS

--- JavaScriptProjects/strings.js
+++ JavaScriptProjects/strings.js
@@ -7,12 +7,17 @@ const description = "cute pup"
 // can dump inner html into the browser directly this way, with backticks we can have mulitple lines unlike // which doesn't allow that
 const myHTML = `
 <div class="wrapper">
-    <h2> Hey ${description}? </h2>
+    <h2> Hey ${description} </h2>
     <img src="${src}" alt="${description}" />
 </div>
 `
 // the above const is a string see console.log below
 console.log(typeof myHTML)
- 
+// b/c myHTML is a string we can't add classList 
+// to change elements in myHTML dynamically we have to first put it in the DOM and then ...
+
 item.innerHTML = myHTML 
-console.log(item.innerHTML)
+const itemImage = document.querySelector(".wrapper img")
+
+itemImage.classList.add("round")
+console.log(itemImage) 
