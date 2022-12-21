3061f79a4dfdba63f3898eb2873746a4f1a7933f
olsikule <olsi.kule@gmail.com>
2022-12-21 12:38:01 -0500

    updated minor details on etch a sketch

--- JavaScriptProjects/Part1_2/etch-a-sketch.js
+++ JavaScriptProjects/Part1_2/etch-a-sketch.js
@@ -1,27 +1,27 @@
 // select elements on the page like canvas, shake buttons
 // the canvas is the element while where we draw is the context
 const canvas = document.querySelector('#etch-a-sketch');
-// commmon naming convention for the context
+// commmon naming convention for the context is ctx
 // there's also 3d canvas
 const ctx = canvas.getContext('2d');
 
 const shakebutton = document.querySelector('.shake');
 
 // when is a true const that will never change use uppercase and underscore _
-const MOVE_AMOUNT = 30;
+const MOVE_AMOUNT = 20;
 
 // setup our canvas for drawing
-// creating a random spot on the canvas
-// make a variable called height and width fromt he same properties of our canvas
+// make a variable called height and width from the same properties of our canvas
 const width = canvas.width;
 const height = canvas.height;
-
-let x = Math.floor(Math.random() * width);
-let y = Math.floor(Math.random() * height);
 
 // the above can also be done with destructuring
 // const { width, height } = canvas;
 console.log(width, height);
+
+// creating a random spot on the canvas
+let x = Math.floor(Math.random() * width);
+let y = Math.floor(Math.random() * height);
 
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
@@ -41,19 +41,20 @@ ctx.stroke();
 function draw({ key }) {
   // increment the hue
   hue = hue + 10;
+// adding a random colour to the line
   ctx.strokeStyle = `hsl(${Math.random() * 360}, 100%, 50%)`;
   console.log(key);
   // start the path
   ctx.beginPath();
-  ctx.moveTo(x, y);
   // move our x and y values depending on what he user did
+  ctx.moveTo(x, y);
 
   // switch statement : take in a varible like a key and depending on the case do the following
-  // this is the same as an if statement (but more readable)
+  // this is the same as an if statement (but easier to read)
   switch (key) {
     case 'ArrowUp':
       y = y - MOVE_AMOUNT;
-      // after each case run the break keyword so it can stop running
+      // after each case run "break" keyword so it can stop running
       break;
     case 'ArrowDown':
       y = y + MOVE_AMOUNT;
@@ -80,16 +81,19 @@ function handleKey(event) {
   }
 }
 
-// clar /shake fxn
+// clear / shake fxn
 function clearCanvas() {
   canvas.classList.add('shake');
+//   clear canvar with this method
   ctx.clearRect(0, 0, width, height);
   canvas.addEventListener(
     'animationend',
     function () {
+        // remove shake from eventlistener
       console.log('Done with the shake.');
       canvas.classList.remove('shake');
     },
+    // run eventListener only once
     { once: true }
   );
 } 
