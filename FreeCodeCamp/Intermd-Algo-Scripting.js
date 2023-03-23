fac44ded16182f47382c2c33eca43a95b5e0e26a
olsikule <olsi.kule@gmail.com>
2023-03-23 12:03:06 -0400

    Diff Two Arrays exercise

--- FreeCodeCamp/Intermd-Algo-Scripting.js
+++ FreeCodeCamp/Intermd-Algo-Scripting.js
@@ -61,3 +61,37 @@ function sumAll(arr) {
   }
   
   sumAll([1, 4]);
+
+  /*  Diff Two Arrays
+
+Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.
+
+Note: You can return the array with its elements in any order.
+
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]) should return an array.
+Passed: ["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return ["pink wool"].
+Passed: ["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return an array with one item.
+Passed: ["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return ["diorite", "pink wool"].
+Passed: ["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return an array with two items.
+Passed: ["andesite", "grass", "dirt", "dead shrub"], ["andesite", "grass", "dirt", "dead shrub"] should return [].
+Passed: ["andesite", "grass", "dirt", "dead shrub"], ["andesite", "grass", "dirt", "dead shrub"] should return an empty array.
+Passed: [1, 2, 3, 5], [1, 2, 3, 4, 5] should return [4].
+Passed: [1, 2, 3, 5], [1, 2, 3, 4, 5] should return an array with one item.
+Passed: [1, "calf", 3, "piglet"], [1, "calf", 3, 4] should return ["piglet", 4].
+Passed: [1, "calf", 3, "piglet"], [1, "calf", 3, 4] should return an array with two items.
+Passed: [], ["snuffleupagus", "cookie monster", "elmo"] should return ["snuffleupagus", "cookie monster", "elmo"].
+Passed: [], ["snuffleupagus", "cookie monster", "elmo"] should return an array with three items.
+Passed: [1, "calf", 3, "piglet"], [7, "filly"] should return [1, "calf", 3, "piglet", 7, "filly"].
+Passed: [1, "calf", 3, "piglet"], [7, "filly"] should return an array with six items.*/ 
+
+function diffArray(arr1, arr2) {
+
+  const longArr = [...arr1, ...arr2];
+  // console.log(longArr);
+  const uniqueArr = longArr.filter((item, index) => longArr.indexOf(item) === index && longArr.lastIndexOf(item) === index);
+  // console.log(uniqueArr)
+  return uniqueArr;
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
