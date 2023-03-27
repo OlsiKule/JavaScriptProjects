e073c7145766c2ad606effdb22a0f44b7d07cf1f
olsikule <olsi.kule@gmail.com>
2023-03-27 12:08:44 -0400

    intermediate algo scripting update

--- FreeCodeCamp/Intermd-Algo-Scripting.js
+++ FreeCodeCamp/Intermd-Algo-Scripting.js
@@ -126,3 +126,12 @@ function diffArray(arr1, arr2) {
 }
 
 diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
+// suggested sln
+function diffArray(arr1, arr2) {
+  return [...diff(arr1, arr2), ...diff(arr2, arr1)];
+
+  function diff(a, b) {
+    return a.filter(item => b.indexOf(item) === -1);
+  }
+}
