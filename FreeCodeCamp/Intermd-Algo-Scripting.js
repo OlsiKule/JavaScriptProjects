a937c051d583607804b33573f58392135d09beab
olsikule <olsi.kule@gmail.com>
2023-04-08 13:22:42 -0400

    new algo challenge

--- FreeCodeCamp/Intermd-Algo-Scripting.js
+++ FreeCodeCamp/Intermd-Algo-Scripting.js
@@ -190,4 +190,5 @@ function whatIsInAName(collection, source) {
     });
   }
   
+  
   whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
