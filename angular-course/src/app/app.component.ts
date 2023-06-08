9ec6f09e09302f1ddd6a30a919c9126c72855eeb
olsikule <olsi.kule@gmail.com>
2023-06-08 11:11:21 -0400

    final updates

--- angular-course/src/app/app.component.ts
+++ angular-course/src/app/app.component.ts
@@ -1,22 +1,57 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
+import {COURSES} from '../db-data';
+import {Course} from './model/course';
+import {CourseCardComponent} from './course-card/course-card.component';
+import {HighlightedDirective} from './directives/highlighted.directive';
+import {Observable} from 'rxjs';
+import {AppConfig, CONFIG_TOKEN} from './config';
+import {COURSES} from '../db-data';
+import {CoursesService} from './courses/courses.service';
+import {createCustomElement} from '@angular/elements';
+import {CourseTitleComponent} from './course-title/course-title.component';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [
+    CourseCardComponent,
+    CourseImageComponent,
+    NgForOf
+  ],
+  standalone: true
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  data = {
-    title: "Angular Core Deep Dive"
-  };
 
-  onLogoClicked(){
-    alert("Hello World!");
+  courses = COURSES;
+
+  constructor() {
+
   }
 
-  onKeyUp(newTitle:string){
+    ngOnInit() {
 
-    this.data.title = newTitle;
-  }
-};
+        const htmlElement = createCustomElement(CourseTitleComponent, {injector:this.injector});
+
+        customElements.define('course-title', htmlElement);
+
+    }
+
+    onEditCourse() {
+
+            this.courses[1].category = 'ADVANCED';
+
+    }
+
+    save(course: Course) {
+        this.coursesService.saveCourse(course)
+            .subscribe(
+                () => console.log('Course Saved!')
+            );
+    }
+
+
+
+}
--- angular-course/src/main.ts
+++ angular-course/src/main.ts
@@ -1,7 +1,21 @@
+import {enableProdMode, importProvidersFrom} from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+import {bootstrapApplication, BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app/app.component';
+import {provideAnimations} from '@angular/platform-browser/animations';
+import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 
+if (environment.production) {
+  enableProdMode();
+}
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(BrowserModule),
+    provideAnimations(),
+    provideHttpClient(withInterceptorsFromDi())
+  ],
+})
+  .catch(err => console.log(err));
